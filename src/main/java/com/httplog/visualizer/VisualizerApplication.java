d7a671830685a7ec7da695b4f4f2f880e710fc76
Šimon Vacek <simonvacky@email.cz>
2022-05-19 12:16:28 +0200

    Initial commit

---
+++ src/main/java/com/httplog/visualizer/VisualizerApplication.java
@@ -0,0 +1,16 @@
+package com.httplog.visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.net.URL;
+
+@SpringBootApplication
+public class VisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/httplog/visualizer/VisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httplog.visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
